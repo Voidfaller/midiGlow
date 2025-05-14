2bfd6d4ed1a8e352a6f1f2b1570767830f579863
Voidfaller <giovannidegroot@gmail.com>
2025-05-14 22:16:51 +0100

    Enhance build script to include run option; provide feedback on execution status

--- build.sh
+++ build.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
+RUN_FLAG=false
+
+for arg in "$@"; do
+  if [ "$arg" == "--run" ]; then
+    RUN_FLAG=true
+  fi
+done
+
+
 mkdir -p build
 cd build
 cmake ..
 make
+
+if [ $? -eq 0 ]; then
+  echo "Build successful!"
+
+    if [ "$RUN_FLAG" = true ]; then
+        echo "Running the program..."
+        cd build
+        ./midiGlow
+         # Replace with your actual program name
+    else
+        echo "Use --run to execute the program."
+    fi
+else
+  echo "Build failed!"
+  exit 1
+fi
