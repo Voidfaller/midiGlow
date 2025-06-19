f4ea95e1b21eeba8c6bbe4fec7f2e7a3b7eaba5d
Voidfaller <giovannidegroot@gmail.com>
2025-06-19 10:40:44 +0200

    Implement RotaryEncoder class with update, getRotation, and isButtonPressed methods; add GPIO setup and button debounce logic

--- libs/RotaryEncoder/RotaryEncoder.h
+++ libs/RotaryEncoder/RotaryEncoder.h
@@ -1,23 +1,23 @@
 #ifndef RE_H
 #define RE_H
 
+#include <cstdint>
 class RotaryEncoder
 {
 private:
-    /* data */
+    uint8_t pinA, pinB, pinButton;
+    int _lastState;
+    int _rotation;
+    bool _buttonPressed;
+    unsigned long _lastDebounceTime;
+
 public:
-    RotaryEncoder(/* args */);
+    RotaryEncoder(uint8_t pinA, uint8_t pinB, uint8_t pinButton);
     ~RotaryEncoder();
+    void update();
+    int getRotation();
+    bool isButtonPressed();
+
 };
-
-RotaryEncoder::RotaryEncoder(/* args */)
-{
-}
-
-RotaryEncoder::~RotaryEncoder()
-{
-}
-
-
 
 #endif // RE_H
