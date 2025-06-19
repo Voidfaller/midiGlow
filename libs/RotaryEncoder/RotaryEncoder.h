912b394b10f0ee7a309b208c9f8ad139478b01e3
Voidfaller <giovannidegroot@gmail.com>
2025-06-19 10:33:17 +0200

    Add RotaryEncoder library and update CMake configuration

---
+++ libs/RotaryEncoder/RotaryEncoder.h
@@ -0,0 +1,23 @@
+#ifndef RE_H
+#define RE_H
+
+class RotaryEncoder
+{
+private:
+    /* data */
+public:
+    RotaryEncoder(/* args */);
+    ~RotaryEncoder();
+};
+
+RotaryEncoder::RotaryEncoder(/* args */)
+{
+}
+
+RotaryEncoder::~RotaryEncoder()
+{
+}
+
+
+
+#endif // RE_H
